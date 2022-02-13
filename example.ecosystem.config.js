86424d42a1591bd5e6ce57c16a3e25e24d29a788
wist9063 <me@joshlol.xyz>
2022-02-12 22:05:57 -0800

    chore: adding example ecosystem config

---
+++ example.ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+    {
+      name: 'NoLeauge',
+      script: './src/index.js',
+      env: {
+        'NODE_ENV': 'development',
+        'DISCORD_TOKEN': '',
+        'colors': {
+          'blue': '#7289DA',
+          'red_event': '#DD5449',
+          'green_event': '#5cb85c',
+          'blue_event': '#5bc0de'
+        }
+      },
+      env_production: {
+        'NODE_ENV': 'production'
+      }
+    }
+  ]
+};
+  
