43e6e83f2d78f3abfc93165f6926705ceb069464
wist9063 <me@joshlol.xyz>
2022-02-12 22:32:13 -0800

    refactor: commenting out message sending for now

--- src/events/User/presenceUpdate.js
+++ src/events/User/presenceUpdate.js
@@ -14,7 +14,7 @@ module.exports = class extends BotEvent {
     // detect if user is playing Leauge of Legends
     if (newPresence.activities[0] && newPresence.activities[0].name === 'League of Legends') {
       console.log('hey, i see a league of legends player! sending them a message!');
-      newPresence.member.send('Hey, you are playing League of Legends! Stop!');
+      //newPresence.member.send('Hey, you are playing League of Legends! Stop! Or else...');
     }
   }
 };
